6b6cb7b317b835132c2775f52ec5818158181268
DESKTOP-II93BOO\pjs <bavuchoko2naver.com>
2023-06-15 16:01:04 +0900

    gaem add

--- src/main/java/com/pjs/golf/game/repository/GameJpaRepository.java
+++ src/main/java/com/pjs/golf/game/repository/GameJpaRepository.java
@@ -1,6 +1,7 @@
 package com.pjs.golf.game.repository;
 
+import com.pjs.golf.game.entity.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GameJpaRepository extends JpaRepository<GameJpaRepository, Integer> {
+public interface GameJpaRepository extends JpaRepository<Game, Integer> {
 }
